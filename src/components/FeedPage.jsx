043c0e028f60edfb76bedf0eb1c88a8339f8c355
Abdul Imad <abdimad786@gmail.com>
2021-07-11 21:37:14 +0530

    given functionality to navbar and upload file comp

--- src/components/FeedPage.jsx
+++ src/components/FeedPage.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { database } from "../firebase";
 import Navbar from "./Navbar";
 import UploadBtn from "./UploadBtn";
+import VideoFeed from "./VideoFeed";
 
 export default function Feed() {
-	console.log("Feed logout");
+	const [user, setUser] = useState();
+	const [pageLoading, setPageLoading] = useState(true);
+
+	const { currentUser } = useContext(AuthContext);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			let currUserData = await database.users.doc(currentUser.uid).get();
+			setUser(currUserData.data());
+			setPageLoading(false);
+		};
+		fetchUser();
+	}, []);
+
 	return (
 		<div>
-			<Navbar />
-			<UploadBtn />
+			{pageLoading ? (
+				<div>Loading....</div>
+			) : (
+				<>
+					<Navbar user={user} />
+					<UploadBtn />
+					<VideoFeed />
+				</>
+			)}
 		</div>
 	);
 }
