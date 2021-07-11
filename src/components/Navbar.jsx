5f1c95112df0561fc8e1bb15f27d2a24b1c39fb4
Abdul Imad <abdimad786@gmail.com>
2021-07-11 21:36:28 +0530

    get profile image and displayed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React,{useState,useContext} from 'react';
+import { Avatar } from '@material-ui/core';
 import { AuthContext } from "../contexts/AuthContext";
 
-export default function Navbar() {
+export default function Navbar({user}) {
     const [loader, setLoader]=useState(false);
 	let { signOut } = useContext(AuthContext);
+    
 	const logout = async () => {
         setLoader(true);
 		await signOut();
@@ -11,6 +13,7 @@ export default function Navbar() {
 	};
     return (
         <div>
+            <Avatar src={user.profileUrl} alt="Profile-Pic" />
             <button onClick={logout} disabled={loader}>Logout</button>
         </div>
     )
