0111e5e903989f0106e653406ae57f36041c48a2
Abdul Imad <abdimad786@gmail.com>
2021-07-13 02:35:48 +0530

    solved video upload isse

--- src/components/FeedPage.jsx
+++ src/components/FeedPage.jsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import { useContext } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { database } from "../firebase";
 import Navbar from "./Navbar";
@@ -8,7 +6,7 @@ import UploadBtn from "./UploadBtn";
 import VideoFeed from "./VideoFeed";
 
 export default function Feed() {
-	const [user, setUser] = useState();
+	const [userData, setUserData] = useState();
 	const [pageLoading, setPageLoading] = useState(true);
 
 	const { currentUser } = useContext(AuthContext);
@@ -16,7 +14,7 @@ export default function Feed() {
 	useEffect(() => {
 		const fetchUser = async () => {
 			let currUserData = await database.users.doc(currentUser.uid).get();
-			setUser(currUserData.data());
+			setUserData(currUserData.data());
 			setPageLoading(false);
 		};
 		fetchUser();
@@ -28,8 +26,8 @@ export default function Feed() {
 				<div>Loading....</div>
 			) : (
 				<>
-					<Navbar user={user} />
-					<UploadBtn />
+					<Navbar userData={userData} />
+					<UploadBtn userData={userData} />
 					<VideoFeed />
 				</>
 			)}
